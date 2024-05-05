ffe07aee16519837c157690dab01f6e94ddde865
Dong <tandong98123@gmail.com>
2024-05-05 07:30:39 +0700

    first commit

---
+++ src/assets/components/questionCard/QuestionCard.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './QuestionCard.scss'
+
+type props = {
+    question : string;
+    corectAnser: string;
+    userAnswer : string;
+    questionNunber : number;
+    totalQuestion : number;
+}
+function QuestionCart() {
+
+  return (
+    <div></div>
+  )
+}
+
+export default QuestionCart;
